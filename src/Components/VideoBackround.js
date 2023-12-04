6386eb4a890b71933d47a44f67033bbcff147d36
Praveen kumar Bandi <53850436+bpraveendrpt@users.noreply.github.com>
2023-12-04 12:52:26 -0500

    video pay files

--- src/Components/VideoBackround.js
+++ src/Components/VideoBackround.js
@@ -1,23 +1,27 @@
-import React, { useEffect } from 'react'
-import { API_OPTIONS } from '../utils/constants'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import useMovieTrailer from '../Hooks/useMovieTrailer';
 
-const VideoBackround = ({movieId}) => {
+const VideoBackround = ({ movieId }) => {
+  console.log(movieId +"hyd");
 
-    const getMovieVideos = async () =>{
-        const data = await fetch ('https://api.themoviedb.org/3/movie/1075794/videos?language=en-US', 
-        API_OPTIONS
-        )
-        const json = await data.json();
-        console.log(json);
-        const filterData = json.results.filter((vedio) => vedio.type === "Trailer" );
-        const trailer = filterData.length ? filterData[0] : json.results[0];
-        console.log(trailer);
-    }
-    useEffect ( () =>{
-        getMovieVideos();
-    },[])
+
+    const  trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+    useMovieTrailer(movieId); 
+    
   return (
-    <div>VideoBackround</div>
+    <div className=' w-screen'>
+      <iframe 
+      className=' w-screen aspect-video' 
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        title="YouTube video player" 
+        
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        allowFullScreen>
+
+      </iframe>
+
+    </div>
   )
 }
 
