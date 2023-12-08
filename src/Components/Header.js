981ebc69c63486b27d65a4e6fd6040b17215ea03
Praveen kumar Bandi <53850436+bpraveendrpt@users.noreply.github.com>
2023-12-08 02:02:51 -0500

    updated files

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -55,7 +55,7 @@ const Header = () => {
         }
 
     return(
-        <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between"> 
+        <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between bg-black sm: bg-black md: bg-black"> 
             <img 
             className="w-44" 
             src={logo}
